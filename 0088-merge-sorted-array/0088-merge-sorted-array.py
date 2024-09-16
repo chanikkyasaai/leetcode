86c0f7ea20335e89893e05588b5de01237ab873e
Nelapatla Chanikya Sai <125785223+chanikkyasaai@users.noreply.github.com>
2024-09-16 17:23:24 +0530

    Time: 37 ms (74.66%), Space: 16.7 MB (8.71%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,33 +1,55 @@
+import math
+
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    #     for i in range(m, m+n):
+    #         nums1[i] = nums2[i-m]
+
+    #     nums1 = self.quicksort(nums1, 0, len(nums1)-1)
+
+    # def partition(self, arr, lb, ub):
+    #     pivot = arr[lb]
+    #     start = lb
+    #     end = ub
+
+    #     while start< end:
+
+    #         while start <= ub and arr[start] <= pivot:
+    #             start+=1
+    #         while arr[end] > pivot:
+    #             end -=1
+    #         if start < end:
+    #             arr[start], arr[end] = arr[end], arr[start]
+
+    #     arr[lb], arr[end] = arr[end], arr[lb]
+    #     return end
+
+    
+    # def quicksort(self, arr, lb,  ub):
+    #     if lb < ub:
+    #         loc = self.partition(arr, lb, ub)
+    #         self.quicksort(arr, lb, loc-1)
+    #         self.quicksort(arr, loc+1, ub)
+
+    #     return arr
+
         for i in range(m, m+n):
             nums1[i] = nums2[i-m]
 
-        nums1 = self.quicksort(nums1, 0, len(nums1)-1)
+        # applying gap method
 
-    def partition(self, arr, lb, ub):
-        pivot = arr[lb]
-        start = lb
-        end = ub
+        gap = math.ceil((m+n)/2)
 
-        while start< end:
+        while gap >=1 :
+            left = 0
+            right = left + gap
 
-            while start <= ub and arr[start] <= pivot:
-                start+=1
-            while arr[end] > pivot:
-                end -=1
-            if start < end:
-                arr[start], arr[end] = arr[end], arr[start]
-
-        arr[lb], arr[end] = arr[end], arr[lb]
-        return end
-
-    
-    def quicksort(self, arr, lb,  ub):
-        if lb < ub:
-            loc = self.partition(arr, lb, ub)
-            self.quicksort(arr, lb, loc-1)
-            self.quicksort(arr, loc+1, ub)
-
-        return arr
-        
+            while left < m+n and right < m+n:
+                if nums1[left] > nums1[right]:
+                    nums1[left], nums1[right] = nums1[right], nums1[left]
+                left +=1
+                right+=1
+            
+            if gap == 1:
+                break
+            gap = math.ceil(gap / 2)
