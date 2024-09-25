3f5ff97e05ac34887b75a5b4b137e2612b48b9ac
Nelapatla Chanikya Sai <125785223+chanikkyasaai@users.noreply.github.com>
2024-09-25 21:17:21 +0530

    Time: 1853 ms (56.9%), Space: 305.3 MB (81.38%) - LeetHub

--- 2494-sum-of-prefix-scores-of-strings/2494-sum-of-prefix-scores-of-strings.py
+++ 2494-sum-of-prefix-scores-of-strings/2494-sum-of-prefix-scores-of-strings.py
@@ -1,20 +1,58 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.count = 0
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            
+            node = node.children[char]
+            node.count+=1
+    
+    def search(self, word):
+        node = self.root
+        score = 0
+
+        for char in word:
+            if char not in node.children:
+                return 0
+            node = node.children[char]
+            score+= node.count
+        
+        return score
+
+
 class Solution:
     def sumPrefixScores(self, words: List[str]) -> List[int]:
         
-        ans = [0] * len(words)
+        # ans = [0] * len(words)
 
-        prefixscore = {}
+        # prefixscore = {}
 
-        for word in words:
-            for i in range(1,len(word)+1):
-                if word[:i] in prefixscore:
-                    prefixscore[word[:i]] +=1
-                else:
-                    prefixscore[word[:i]] =1
+        # for word in words:
+        #     for i in range(1,len(word)+1):
+        #         if word[:i] in prefixscore:
+        #             prefixscore[word[:i]] +=1
+        #         else:
+        #             prefixscore[word[:i]] =1
 
-        for i,word in enumerate(words):
-            for j in range(1,len(word)+1):
-                ans[i]+= prefixscore[word[:j]]
+        # for i,word in enumerate(words):
+        #     for j in range(1,len(word)+1):
+        #         ans[i]+= prefixscore[word[:j]]
 
         
-        return ans
+        # return ans
+
+        trie = Trie()
+        for word in words:
+            trie.insert(word)
+        
+        return [trie.search(word) for word in words]
+
