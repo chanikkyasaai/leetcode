185ef645c70afe093cc63617640cdfd7780de1d0
Nelapatla Chanikya Sai <125785223+chanikkyasaai@users.noreply.github.com>
2024-10-02 23:16:59 +0530

    Time: 57 ms (42.2%), Space: 16.6 MB (45.03%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -7,3 +7,11 @@ class Solution:
             if num[i] != num[n-i-1]:
                 return False
         return True
+        # if x < 0:
+        #     return False
+        # nx = str(x)
+        # num = int(nx[::-1])
+        # if x == num:
+        #     return True
+        # else:
+        #     return False
