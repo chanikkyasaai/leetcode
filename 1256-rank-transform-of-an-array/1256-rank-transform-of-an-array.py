b265253b23bd7bea6cba0afcece22a5d1f25afac
Nelapatla Chanikya Sai <125785223+chanikkyasaai@users.noreply.github.com>
2024-10-02 11:34:52 +0530

    Time: 253 ms (30.89%), Space: 33.2 MB (75.1%) - LeetHub

---
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        if len(arr) == 0:
+            return []
+
+        sortedarr = sorted(arr)
+        rankdict = {}
+        i=0
+
+        for  j in range(len(arr)):
+            if sortedarr[j] in rankdict:
+                continue
+            i+=1
+            rankdict[sortedarr[j]] = i
+
+        for j in range(len(arr)):
+            arr[j] = rankdict[arr[j]]
+
+        return arr
+        
+            
