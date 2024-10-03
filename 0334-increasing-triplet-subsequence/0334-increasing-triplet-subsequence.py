7dd40fc358ad3ae2459f779f44acaa11c1a8c237
Nelapatla Chanikya Sai <125785223+chanikkyasaai@users.noreply.github.com>
2024-10-03 22:31:30 +0530

    Time: 822 ms (24.51%), Space: 32.7 MB (18.93%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,5 +1,8 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
+        if len(nums) < 3:
+            return False
+
         min0 = float('inf')
         min1 = float('inf')
 
@@ -7,7 +10,7 @@ class Solution:
             if num < min0:
                 min0 = num
             elif num > min0:
-                min1 = min(min1,num)
+                min1 = min(min1, num)
             if num > min1:
                 return True
         
