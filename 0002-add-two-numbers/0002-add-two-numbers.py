d8b1a2e4188b839333431287833378fce45c975a
Nelapatla Chanikya Sai <125785223+chanikkyasaai@users.noreply.github.com>
2025-07-29 18:03:59 +0530

    Time: 0 ms (100%), Space: 18 MB (14.77%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,34 +1,27 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        carry = 0
-        ans = ListNode()
-        nd = ans
+        dummy = ListNode()
+        ans = dummy
+        carry  = 0
 
-        while (l1 or l2) or carry:
-            sum = 0
+        while l1 != None or l2 != None or carry!=0:
+            x = l1.val if l1 != None else 0
+            y = l2.val if l2 != None else 0
+            sm = x+y+carry
+    
+            carry = sm//10
 
-            if l1:
-                sum += l1.val
+            dummy.next = ListNode(sm%10)
+            dummy = dummy.next
+
+            if l1 != None:
                 l1 = l1.next
-            if l2:
-                sum+= l2.val
+            if l2!= None:
                 l2 = l2.next
-            
-            if carry:
-                sum+= carry
-
-            carry = sum // 10
-            sum = sum%10
-
-            node = ListNode(sum)
-            nd.next = node
-
-            nd = nd.next
 
         return ans.next
