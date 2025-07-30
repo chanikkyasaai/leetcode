0c77aa16f647883249bb9397cd0d774288217f93
Nelapatla Chanikya Sai <125785223+chanikkyasaai@users.noreply.github.com>
2025-07-30 13:50:29 +0530

    Time: 480 ms (82.11%), Space: 20.7 MB (36.48%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,49 +1,30 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # ans = set()
-        # n = len(nums)
-
-        # if nums.count(0) == n:
-        #     return [[0,0,0]]
-
-        # for i in range(n):
-
-        #     target = -nums[i]
-        #     key_dict = { }
-
-        #     for j in range(i+1,n):
-        #         complement = target - nums[j]
-        #         if complement in key_dict:
-        #                 ls = (nums[i], nums[j], complement)
-        #                 ans.add(tuple(sorted(ls)))
-
-        #         key_dict[nums[j]] = j
-
-        ans = []
-        n = len(nums)
         nums.sort()
-
-        for i in range(n):
-            if i >0 and nums[i] == nums[i-1]:
+        l = len(nums)
+        ans = []
+        for i in range(l):
+            if i>0 and nums[i-1]==nums[i]:
                 continue
+            if i>=l-1:
+                break
+            left = i+1
+            right = l -1
+            target = -nums[i]
+    
+            while left < right:
+                sm = nums[left] + nums[right]
+                if target == sm:
+                    ans.append([nums[i],nums[left], nums[right]])
 
-            j,k = i+1,n-1
-            while j < k:
-                total = nums[i] + nums[j] + nums[k]
-
-                if total == 0:
-                    ans.append([nums[i],nums[j], nums[k]])
-                    j+=1
-                    k-=1
-
-                    while j < k and nums[j] == nums[j-1]:
-                        j+=1
-                    while j < k and nums[k] == nums[k+1]:
-                        k-=1
-
-                elif total <0:
-                    j+=1
-                else:
-                    k-=1  
-
+                    while left < right and nums[left] == nums[left+1]:
+                        left+=1
+                    while left < right and nums[right] == nums[right-1]:
+                        right-=1
+                    left+=1
+                    right-=1
+                elif sm < target:
+                    left+=1
+                else :
+                    right-=1
         return ans
