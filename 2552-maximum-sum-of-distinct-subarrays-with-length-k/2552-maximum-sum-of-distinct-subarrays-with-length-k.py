849fc1663250b536a0f3a5b9dd11635ae7d34c39
Nelapatla Chanikya Sai <125785223+chanikkyasaai@users.noreply.github.com>
2025-07-31 08:24:24 +0530

    Time: 91 ms (88.38%), Space: 34.2 MB (97.03%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,22 +1,24 @@
+from collections import Counter
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        i, j, curS, maxS = 0, 0, 0, 0
-        num_set = set()
-        
-        while j < len(nums):
-            while nums[j] in num_set:
-                num_set.remove(nums[i])
-                curS -= nums[i]
-                i += 1
-                
-            num_set.add(nums[j])
-            curS += nums[j]
+        m = len(nums)
+        left = 0
+        win = set()
+        maxsum = 0
+        cursum = 0
+
+        for right in range(m):
+            while nums[right] in win:
+                win.remove(nums[left])
+                cursum-=nums[left]
+                left+=1
             
-            if j - i + 1 == k:
-                maxS = max(maxS, curS)
-                num_set.remove(nums[i])
-                curS -= nums[i]
-                i += 1
-            j += 1
-        
-        return maxS
+            win.add(nums[right])
+            cursum += nums[right]
+            
+            if right-left+1 == k:
+                maxsum = max(maxsum, cursum)
+                win.remove(nums[left])
+                cursum-=nums[left]
+                left+=1
+        return maxsum
