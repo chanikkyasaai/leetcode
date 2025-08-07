a02aca3ca2988742db3868478070c48013c6f686
Nelapatla Chanikya Sai <125785223+chanikkyasaai@users.noreply.github.com>
2025-08-07 15:51:32 +0530

    Time: 11 ms (18.25%), Space: 19.7 MB (42.84%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,10 +1,19 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        seen = set()
-        for i in nums:
-            if i in seen:
-                seen.remove(i)
-            else:
-                seen.add(i)
+        # seen = set()
+        # for i in nums:
+        #     if i in seen:
+        #         seen.remove(i)
+        #     else:
+        #         seen.add(i)
         
-        return list(seen)[0]
+        # return list(seen)[0]
+
+        nums.sort()
+        i= 0
+        while i<len(nums)-1:
+            if nums[i]==nums[i+1]:
+                i+=2
+            else:
+                return nums[i]
+        return nums[i]
