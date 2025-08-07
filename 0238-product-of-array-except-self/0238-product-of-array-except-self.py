12b854cb18432a97f806842bd6b85406e2ad0e95
Nelapatla Chanikya Sai <125785223+chanikkyasaai@users.noreply.github.com>
2025-08-07 11:51:06 +0530

    Time: 47 ms (9.27%), Space: 26.1 MB (26.47%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,16 +1,24 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        fwd = []
+        fdp =  1
+        for i in nums:
+            fdp *= i
+            fwd.append(fdp)
+        bwd = []
+        bdp = 1
+        for i in reversed(nums):
+            bdp*= i
+            bwd.append(bdp)
+        bwd.reverse()
+        #1,2,6,24 -- 
         n = len(nums)
-        prepro = 1
-        pospro = 1
-        res = [0]*n
-
+        res = [1]*n
         for i in range(n):
-            res[i]= prepro
-            prepro *= nums[i]
-
-        for j in range(n-1,-1,-1):
-            res[j]*= pospro
-            pospro *=nums[j]
-
+            if i-1>=0:
+                res[i]*= fwd[i-1]
+            if i+1 < n:
+                res[i]*= bwd[i+1]
+        
         return res
+
