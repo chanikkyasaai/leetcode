1f28f76807b03fafe18cdc142fd32c62bcc6ff4c
Nelapatla Chanikya Sai <125785223+chanikkyasaai@users.noreply.github.com>
2025-08-19 21:45:08 +0530

    Time: 273 ms (27.84%), Space: 28.8 MB (8.51%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,27 +5,22 @@ class Solution:
         n = len(grid[0])
         islandCount = 0
 
-        def bfs(i,j):
+        def dfs(i,j):
+            if i <0 or i>= m or j <0 or j >= n or (i,j) in visited or grid[i][j] == "0":
+                return
+
             visited.add((i,j))
-            if 0 <= i-1 <= m-1 and 0<= j <= n-1 and grid[i-1][j] == "1" and (i-1,j) not in visited:
-                bfs(i-1,j)
-            if 0 <= i <= m-1 and 0<= j-1 <= n-1 and grid[i][j-1] == "1" and (i,j-1) not in visited:
-                bfs(i,j-1)
-            if 0 <= i+1 <= m-1 and 0<= j <= n-1 and grid[i+1][j] == "1" and (i+1,j) not in visited:
-                bfs(i+1,j)
-            if 0 <= i <= m-1 and 0<= j+1 <= n-1 and grid[i][j+1] == "1" and (i,j+1) not in visited:
-                bfs(i,j+1)
-            return
+
+            dfs(i-1,j)
+            dfs(i,j-1)
+            dfs(i+1,j)
+            dfs(i,j+1)
             
 
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == "0":
-                    continue
-                else:
-                    if (i,j) in visited:
-                        continue
-                    bfs(i,j)
+                if grid[i][j] == "1" and (i,j) not in visited:
+                    dfs(i,j)
                     islandCount+=1
 
         return islandCount
